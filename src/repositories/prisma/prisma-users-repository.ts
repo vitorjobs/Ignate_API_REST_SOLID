a8843e5c4d02fafbede7edb28d889ec02ecf8e7b
Vitor Guedes <vittorjobs@gmail.com>
2025-04-19 21:15:54 -0300

    ✨ feat(profile): add get profile by ID service
    - Implemented profile lookup feature:
      • New use-case: GetUserProfileUseCase
      • Repository method: findById()

--- src/repositories/prisma/prisma-users-repository.ts
+++ src/repositories/prisma/prisma-users-repository.ts
@@ -1,21 +1,25 @@
 import { prisma } from "lib/prisma";
-import {Prisma} from "@prisma/client"
+import { Prisma, User } from "@prisma/client"
 import { UsersRepository } from "../users-repository";
 
 export class PrismaUsersRepository implements UsersRepository {
-   async findByEmail(email: string) {
-     
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  findById(id: string): Promise<User | null> {
+    throw new Error("Method not implemented.");
+  }
+  async findByEmail(email: string) {
+
     const user = await prisma.user.findUnique({
       where: {
         email
       }
     })
     return user
-   }
-   async create(data: Prisma.UserCreateInput){
+  }
+  async create(data: Prisma.UserCreateInput) {
     const user = await prisma.user.create({
       data,
     })
     return user
-   }
+  }
 }
