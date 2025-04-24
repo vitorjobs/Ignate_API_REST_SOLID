a9727f5b2a24d9f6c266222807e5dfcbc2a89964
Vitor Guedes <vittorjobs@gmail.com>
2025-04-23 22:32:13 -0300

    ✨ feat(check-in): implement daily check-in limit per gym
    - Added business rule:
      • Max 1 check-in per user per gym per day
      • Returns Erro Conflict if limit exceeded
    
    - Implementation:
      ├─ New repository method:
        │  • findByUserIdOnDate(userId, date)
      ├─ Validation in CheckInUseCase:
        │  • Checks existing check-ins
        │  • Throws MaxNumberOfCheckInsError

--- src/use-cases/check-in.spec.ts
+++ src/use-cases/check-in.spec.ts
@@ -1,5 +1,5 @@
 
-import { describe, it, beforeEach, expect } from 'vitest'
+import { describe, it, beforeEach, expect, vi, afterEach } from 'vitest'
 import { InMemoryCheckInsRepository } from 'repositories/in-memory/in-memory-check-ins-repository'
 import { CheckInUseCase } from './checkin'
 
@@ -10,10 +10,45 @@ describe('Check-in Use Case', () => {
   beforeEach(() => {
     checkInsRepository = new InMemoryCheckInsRepository()
     sut = new CheckInUseCase(checkInsRepository)
+    vi.useFakeTimers()
 
   })
 
+  afterEach(() => {
+    vi.useRealTimers()
+  })
+
   it('should be able to Check-in', async () => {
+    const { checkIn } = await sut.execute({
+      userId: "gym-01",
+      gymId: "user-01",
+    })
+    expect(checkIn.id).toEqual(expect.any(String))
+  })
+
+  it('should note be able to Check in twice in the same day', async () => {
+    vi.setSystemTime(new Date(2022, 0, 20, 18, 0, 0))
+
+    await sut.execute({
+      userId: "gym-01",
+      gymId: "user-01",
+    })
+    await expect(() => sut.execute({
+      userId: "gym-01",
+      gymId: "user-01",
+    })).rejects.toBeInstanceOf(Error)
+  })
+
+  it('should be able to Check in twice but in different days', async () => {
+    vi.setSystemTime(new Date(2022, 0, 20, 18, 0, 0))
+
+    await sut.execute({
+      userId: "gym-01",
+      gymId: "user-01",
+    })
+
+    vi.setSystemTime(new Date(2021, 0, 20, 18, 0, 0))
+
     const { checkIn } = await sut.execute({
       userId: "gym-01",
       gymId: "user-01",
--- src/use-cases/checkin.ts
+++ src/use-cases/checkin.ts
@@ -19,6 +19,14 @@ export class CheckInUseCase {
     userId, gymId
   }: CheckInUseCaseRequest): Promise<CheckInUseCaseResponse> {
 
+    const checkInOnSameDate = await this.checkInsRepository.findByUserIdOnDate(
+      userId,
+      new Date()
+    )
+
+    if (checkInOnSameDate) {
+      throw new Error()
+    }
     const checkIn = await this.checkInsRepository.create({
       gym_id: userId,
       user_id: gymId,
